ebd53071665ffedae288768403901044f6044368
Alexis Reyes <alexisreyes@quickenloans.com>
2015-12-03 21:55:25 -0500

    added example for using jquery to hit the api with stuff in it

---
+++ WeddingSiteBuilder/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WeddingSiteBuilder.Controllers
+{
+    public class LoginController : ApiController
+    {
+        // GET: api/Login
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Login?userName=dummy&password=pword
+        public bool Get(string userName, string password)
+        {
+            if(userName == "alexis" && password == "reyes")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // POST: api/Login
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Login/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Login/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
