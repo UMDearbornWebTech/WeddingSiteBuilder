64fd1a0a690b867fac50b3d8e0c41df4051179a2
Alexis Reyes <alexisreyes@quickenloans.com>
2015-12-06 11:57:45 -0500

    added email service

---
+++ WeddingSiteBuilder/Services/EmailService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Mail;
+using System.Configuration;
+using WeddingSiteBuilder.Services.Interfaces;
+
+namespace WeddingSiteBuilder.Services
+{
+    public class EmailService : IEmailService
+    {
+        public static IEmailService Instance;
+
+        private static SmtpClient _client;
+
+        static EmailService()
+        {
+            Instance = new EmailService();
+        }
+
+        private EmailService()
+        {
+            _client = new SmtpClient("smtp.gmail.com");
+            _client.EnableSsl = true;
+            _client.Port = 587;
+            _client.Credentials = new NetworkCredential("weddingsitebuilder@gmail.com",
+               ConfigurationManager.AppSettings["emailPassword"].ToString());
+        }
+
+        public void SendEmail(string emailAddress, string subject, string message)
+        {
+            _client.Send("donot-reply@weddingsitebuilder.com", emailAddress, subject, message);
+        }
+    }
+}
---
+++ WeddingSiteBuilder/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeddingSiteBuilder.Services.Interfaces
+{
+    public interface IEmailService
+    {
+        void SendEmail(string emailAddress, string subject, string message);
+    }
+}
