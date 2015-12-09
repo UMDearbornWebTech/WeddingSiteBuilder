c13725581a0c52bcf03332a7ce1c2c796df33ced
Alexis Reyes <alexisreyes@quickenloans.com>
2015-12-09 01:08:53 -0500

    committing for now so I can go to sleep and work on this tomorrow UGHGHGU SO TIRED!

---
+++ WeddingSiteBuilder/Controllers/RSVPController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WeddingSiteBuilder.DTOs;
+using WeddingSiteBuilder.ReadModel;
+
+namespace WeddingSiteBuilder.Controllers
+{
+    public class RSVPController : ApiController
+    {
+        // GET: api/RSVP
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/RSVP/5
+        public List<AttendeeWithRSVP> Get(long WeddingId)
+        {
+            if (WeddingId != 0)
+            {
+                using (var dbContext = new WeddingSiteBuilderEntities())
+                {
+                    var attendees = new List<AttendeeWithRSVP>();
+
+                    dbContext.Attendees
+                        .Where(a => a.WeddingID == WeddingId && !(a.WeddingRole == "Bride" || a.WeddingRole == "Groom" || a.PartyMember == true))
+                        .ToList()
+                        .ForEach(a =>
+                        {
+                            var attendee = (AttendeeWithRSVP)a;
+                            attendee.RSVP = dbContext.RSVPLinks.FirstOrDefault(r => r.AttendeeID == a.AttendeeID);
+                            attendees.Add(attendee);
+                        });
+                    return attendees;
+                }
+            }
+            else
+            {
+                return new List<AttendeeWithRSVP>();
+            }
+        }
+
+        // POST: api/RSVP
+        public void Post([FromBody]string value)
+        {
+
+        }
+
+        // PUT: api/RSVP/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/RSVP/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
