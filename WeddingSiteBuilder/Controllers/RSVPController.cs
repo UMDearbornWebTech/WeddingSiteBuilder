b14f0d673a8447b535821f5618aae500c6f7ef85
Alexis Reyes <alexisreyes@quickenloans.com>
2015-12-10 00:36:37 -0500

    don't need this

--- WeddingSiteBuilder/Controllers/RSVPController.cs
+++ WeddingSiteBuilder/Controllers/RSVPController.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using WeddingSiteBuilder.DTOs;
 using WeddingSiteBuilder.ReadModel;
