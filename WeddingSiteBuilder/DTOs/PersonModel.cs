0dc3847c5d101621f410d81c661534d9595b9558
Alexis Reyes <alexisreyes@quickenloans.com>
2015-12-10 02:23:43 -0500

    got both rsvp send/view and respond to working

---
+++ WeddingSiteBuilder/DTOs/PersonModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WeddingSiteBuilder.ReadModel;
+
+namespace WeddingSiteBuilder.DTOs
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public PersonModel(Person person)
+        {
+            FirstName = person.FirstName;
+            LastName = person.LastName;
+            Email = person.Email;
+        }
+    }
+}
